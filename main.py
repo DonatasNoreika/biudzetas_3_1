5dda1c993bd9b185941a5d2a9dd4e9630788c4bc
Donatas Noreika <donoras@gmail.com>
2023-12-05 11:39:27 +0200

    Sukurtas models.py failas, ten įdėti modeliai

--- main.py
+++ main.py
@@ -1,37 +1,6 @@
 import pickle
 
-
-class Irasas:
-    def __init__(self, suma):
-        self.suma = abs(suma)
-
-
-class PajamuIrasas(Irasas):
-    def __init__(self, suma, siuntejas, info):
-        super().__init__(suma)
-        self.siuntejas = siuntejas
-        self.info = info
-
-    def __str__(self):
-        return f"Pajamos: {self.suma} ({self.siuntejas}, {self.info})"
-
-    def __repr__(self):
-        return f"Pajamos: {self.suma} ({self.siuntejas}, {self.info})"
-
-
-class IslaiduIrasas(Irasas):
-    def __init__(self, suma, budas, isigyta, info):
-        super().__init__(suma)
-        self.budas = budas
-        self.isigyta = isigyta
-        self.info = info
-
-    def __str__(self):
-        return f"Išlaidos: {self.suma} ({self.budas}, {self.isigyta}, {self.info})"
-
-    def __repr__(self):
-        return f"Išlaidos: {self.suma} ({self.budas}, {self.isigyta}, {self.info})"
-
+from models import PajamuIrasas, IslaiduIrasas
 
 class Biudzetas:
     def __init__(self):
